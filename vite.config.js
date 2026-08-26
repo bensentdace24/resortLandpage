45b414ebf9e78b85d7a69c495971fac4bda480b9
Vincent Dave Awa <davevincentawa8@gmail.com>
2026-08-26 08:34:04 +0800

    Initial Samal Sands resort presentation

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
+  base: "/samal-sands-preview/",
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
   },
 });
